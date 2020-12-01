30b519be3ad1dc70ea61bb476981f8eac37ee4fd
Rickard <rickard.forslund@iths.se>
2020-12-01 13:08:39 +0100

    Added user entity with constructor

---
+++ src/main/java/com/shoppiq/entity/Order.java
@@ -0,0 +1,2 @@
+package com.shoppiq.entity;public class Order {
+}
---
+++ src/main/java/com/shoppiq/entity/user.java
@@ -0,0 +1,54 @@
+package com.shoppiq.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+
+
+@Data
+@Entity
+@EqualsAndHashCode
+@NoArgsConstructor
+public class user {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+
+    @NotEmpty
+    private String username;
+
+    @NotEmpty
+    private String password;
+
+    @NotEmpty
+    private String email;
+
+
+    private String phone;
+
+    private Address address;
+
+    private List<Order> orders;
+
+    private List<Item> items;
+
+
+    public user(@NotEmpty String username, @NotEmpty String password, @NotEmpty String email, String phone, Address address) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+
+
+}
+
