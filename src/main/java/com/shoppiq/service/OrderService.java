fc4bd39f050ebf93e6b1ba2895a0508ef9c603bf
toffe <toffe950301@gmail.com>
2020-12-01 13:52:34 +0100

    Added OrderService with methods ffor saving and finding users

--- src/main/java/com/shoppiq/service/OrderService.java
+++ src/main/java/com/shoppiq/service/OrderService.java
@@ -1,2 +1,29 @@
-package com.shoppiq.service;public class OrderService {
+package com.shoppiq.service;
+
+import com.shoppiq.entity.Order;
+import com.shoppiq.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public Order saveOrder(Order order) {
+        return orderRepository.save(order);
+    }
+
+    public Iterable<Order> findAllOrder() {
+        return orderRepository.findAll();
+    }
+
+    public Optional<Order> findById(Long id) {
+        return orderRepository.findById(id);
+    }
 }
