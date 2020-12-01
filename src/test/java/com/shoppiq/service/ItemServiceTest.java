5584596528597cf6bf734919632d6009ff9f8e7c
Rickard <rickard.forslund@iths.se>
2020-12-01 12:43:32 +0100

    created tests for itemservice

---
+++ src/test/java/com/shoppiq/service/ItemServiceTest.java
@@ -0,0 +1,47 @@
+package com.shoppiq.service;
+
+import com.shoppiq.entity.Item;
+import com.shoppiq.enums.Category;
+import com.shoppiq.repository.ItemRepository;
+
+import org.junit.jupiter.api.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import javax.persistence.EntityManager;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class ItemServiceTest {
+
+    Item item;
+    @Autowired
+    EntityManager entityManager;
+    @Autowired
+    ItemRepository itemRepository;
+
+    @BeforeEach
+    void setUp() {
+        item = new Item("Stolströja", 99, 1, Category.CLOTHES, "En tröja med ett stols märke.");
+        itemRepository.save(item);
+    }
+
+    @Test
+    void saveItem() {
+        var newItem = itemRepository.save(item);
+        assertEquals(item, newItem);
+    }
+
+    @Test
+    void findAllItems() {
+        assertEquals(itemRepository.findById((long) 1).get(), itemRepository.findAll().iterator().next());
+    }
+
+    @Test
+    @Order(1)
+    void findById() {
+        assertEquals(item, itemRepository.findById((long) 1).get());
+    }
+
+}
