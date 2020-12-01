c0abd156065da91e0be8c2de0fa4b83967f56e9b
Rickard <rickard.forslund@iths.se>
2020-12-01 12:41:17 +0100

    Added equals method

--- src/main/java/com/shoppiq/entity/Item.java
+++ src/main/java/com/shoppiq/entity/Item.java
@@ -2,6 +2,7 @@ package com.shoppiq.entity;
 
 import com.shoppiq.enums.Category;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Min;
@@ -11,9 +12,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
 
 @Data
 @Entity
+@EqualsAndHashCode
 @NoArgsConstructor
 public class Item {
 
@@ -37,10 +41,7 @@ public class Item {
     @NotNull
     Category category;
 
-
-
-
-    public Item(@NotEmpty String name, @Min(1) @NotEmpty double price, @Min(0) @NotNull int quantity,Category category, String description) {
+    public Item(@NotEmpty String name, @Min(1) @NotEmpty double price, @Min(0) @NotNull int quantity, Category category, String description) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
@@ -48,5 +49,16 @@ public class Item {
         this.category = category;
     }
 
-
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return Double.compare(item.price, price) == 0 &&
+                quantity == item.quantity &&
+                Objects.equals(id, item.id) &&
+                Objects.equals(name, item.name) &&
+                Objects.equals(description, item.description) &&
+                category == item.category;
+    }
 }
