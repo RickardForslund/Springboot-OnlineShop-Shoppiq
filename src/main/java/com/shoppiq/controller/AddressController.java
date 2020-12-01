7e980716955df08dad3063528cc1d0040163e763
Rickard <rickard.forslund@iths.se>
2020-12-01 14:22:37 +0100

    changed mapping path in addressController

--- src/main/java/com/shoppiq/controller/AddressController.java
+++ src/main/java/com/shoppiq/controller/AddressController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1/item")
+@RequestMapping("/api/v1/address")
 public class AddressController {
 
     private final AddressService addressService;
