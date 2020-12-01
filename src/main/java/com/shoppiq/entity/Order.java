2162659eb8774858cf3edb99d63e3632e856c090
Rickard <rickard.forslund@iths.se>
2020-12-01 13:17:11 +0100

    Added Order record

--- src/main/java/com/shoppiq/entity/Order.java
+++ src/main/java/com/shoppiq/entity/Order.java
@@ -1,2 +1,14 @@
-package com.shoppiq.entity;public class Order {
+package com.shoppiq.entity;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
+public record Order (User buyer, LocalDateTime orderDate, List<OrderDetails> orderDetails) {
+    public Order {
+        Objects.requireNonNull(buyer);
+        Objects.requireNonNull(orderDate);
+        Objects.requireNonNull(orderDetails);
+    }
 }
