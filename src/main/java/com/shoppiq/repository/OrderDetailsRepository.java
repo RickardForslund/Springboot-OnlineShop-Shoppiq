5e0e43ad03584894efc883ed105ef494bf8baa91
Rickard <rickard.forslund@iths.se>
2020-12-01 14:02:28 +0100

    Added OrderDetailsRepository

--- src/main/java/com/shoppiq/repository/OrderDetailsRepository.java
+++ src/main/java/com/shoppiq/repository/OrderDetailsRepository.java
@@ -1,9 +1,10 @@
 package com.shoppiq.repository;
 
 import com.shoppiq.entity.Item;
+import com.shoppiq.entity.OrderDetails;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ItemRepository extends CrudRepository<Item,Long> {
+public interface OrderDetailsRepository extends CrudRepository<OrderDetails,Long> {
 }
