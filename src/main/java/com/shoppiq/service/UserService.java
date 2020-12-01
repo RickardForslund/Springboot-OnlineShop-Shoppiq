0d744181830ed65746e60f2e1e0c0ec968e5bbfd
Rickard <rickard.forslund@iths.se>
2020-12-01 13:39:15 +0100

    Added UserService, with method to save and find users

--- src/main/java/com/shoppiq/service/UserService.java
+++ src/main/java/com/shoppiq/service/UserService.java
@@ -1,4 +1,31 @@
 package com.shoppiq.service;
 
+import com.shoppiq.entity.User;
+import com.shoppiq.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Iterable<User> findAllItems() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+
 }
