5f482ecf4ef4443f4b5f3ea7567e6c243959bb97
Rickard <rickard.forslund@iths.se>
2020-12-01 13:53:17 +0100

    Added AddressService with save and find methods

--- src/main/java/com/shoppiq/service/AddressService.java
+++ src/main/java/com/shoppiq/service/AddressService.java
@@ -1,2 +1,30 @@
-package com.shoppiq.service;public class AddressService {
+package com.shoppiq.service;
+
+import com.shoppiq.entity.Address;
+import com.shoppiq.repository.AddressRepository;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+public class AddressService {
+
+    private final AddressRepository addressRepository;
+
+    public AddressService(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
+
+    public Address saveAddress(Address address) {
+        return addressRepository.save(address);
+    }
+
+
+    public Iterable<Address> findAllAddress() {
+        return addressRepository.findAll();
+    }
+
+    public Optional<Address> findById(Long id) {
+        return addressRepository.findById(id);
+    }
 }
