97964623f99e31efbf5bcb86e8471d4fb1072161
toffe <toffe950301@gmail.com>
2020-12-02 10:52:35 +0100

    Changed Address from record to class

--- src/main/java/com/shoppiq/entity/Address.java
+++ src/main/java/com/shoppiq/entity/Address.java
@@ -1,12 +1,35 @@
 package com.shoppiq.entity;
 
-import java.util.Objects;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
-public record Address(String country, String city, String streetAddress, String postalCode, String apartmentNumber, String co) {
-    public Address {
-        Objects.requireNonNull(country);
-        Objects.requireNonNull(city);
-        Objects.requireNonNull(streetAddress);
-        Objects.requireNonNull(postalCode);
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotEmpty
+    private String country, city, streetAddress, postalCode;
+    private String apartmentNumber, co;
+
+    public Address(@NotEmpty String country, @NotEmpty String city, @NotEmpty String streetAddress, @NotEmpty String postalCode, String apartmentNumber, String co) {
+        this.country = country;
+        this.city = city;
+        this.streetAddress = streetAddress;
+        this.postalCode = postalCode;
+        this.apartmentNumber = apartmentNumber;
+        this.co = co;
     }
 }
