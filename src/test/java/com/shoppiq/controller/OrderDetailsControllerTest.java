db025c6cb2a7918d988b86c4b25cd57a72f6f87f
Emily Inns <nyssa.inns@gmail.com>
2020-12-02 14:23:03 +0100

    Written tests for OrderDetailsController.

--- src/test/java/com/shoppiq/controller/OrderDetailsControllerTest.java
+++ src/test/java/com/shoppiq/controller/OrderDetailsControllerTest.java
@@ -5,6 +5,7 @@ import com.shoppiq.entity.Item;
 import com.shoppiq.entity.OrderDetails;
 import com.shoppiq.enums.Category;
 import com.shoppiq.repository.ItemRepository;
+import com.shoppiq.repository.OrderDetailsRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,38 +25,61 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class OrderDetailsControllerTest {
 
     OrderDetails orderDetails;
+    OrderDetails orderDetails2;
     Item item;
+    Item item2;
     @Autowired
     MockMvc mockMvc;
     @Autowired
     ObjectMapper objectMapper;
     @Autowired
-    ItemRepository itemRepository;
+    OrderDetailsRepository orderDetailsRepository;
 
     @BeforeEach
     void setUp() {
         item = new Item("Stolströja", 99, 1, Category.CLOTHES, "En tröja med ett stols märke.");
         orderDetails = new OrderDetails(item, 3);
-
+        orderDetailsRepository.save(orderDetails);
+        item2 = new Item("Cat Toy", 50, 1, Category.TOYS, "A feather tied with string to a stick. Cats love it!");
+        orderDetails2 = new OrderDetails(item2, 5);
+        orderDetailsRepository.save(orderDetails2);
     }
 
     @Test
     void saveOrderDetails() throws Exception  {
         mockMvc.perform(post("/api/v1/orderdetails")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(item)))
+                .content(objectMapper.writeValueAsString(orderDetails)))
                 .andExpect(status().isOk())
-              //  .andExpect(jsonPath("item").value(orderDetails.item()))
+                .andExpect(jsonPath("itemId").value(item.getId()))
+                .andExpect(jsonPath("itemName").value("Stolströja"))
                 .andExpect(jsonPath("quantity").value("3"))
                 .andExpect(jsonPath("price").value(297));
 
     }
 
     @Test
-    void findAllOrderDetails() {
+    void findAllOrderDetails() throws Exception {
+        mockMvc.perform(get("/api/v1/orderdetails"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[0].itemName").value("Stolströja"))
+                .andExpect(jsonPath("$[0].price").value(297))
+                .andExpect(jsonPath("$[0].quantity").value(3))
+                .andExpect(jsonPath("$[1].id").value(2))
+                .andExpect(jsonPath("$[1].itemName").value("Cat Toy"))
+                .andExpect(jsonPath("$[1].price").value(250))
+                .andExpect(jsonPath("$[1].quantity").value(5));
+
     }
 
     @Test
-    void findOrderDetailsById() {
+    void findOrderDetailsById() throws Exception {
+        mockMvc.perform(get("/api/v1/orderdetails/2"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[1].id").value(2))
+                .andExpect(jsonPath("$[1].itemName").value("Cat Toy"))
+                .andExpect(jsonPath("$[1].price").value(250))
+                .andExpect(jsonPath("$[1].quantity").value(5));
     }
 }
