ffa602e2ec6c2b9591bf59b991cb898e11d9d390
toffe <toffe950301@gmail.com>
2020-12-02 10:52:17 +0100

    Changed Order from record to class

--- src/main/java/com/shoppiq/entity/Order.java
+++ src/main/java/com/shoppiq/entity/Order.java
@@ -1,13 +1,34 @@
 package com.shoppiq.entity;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Objects;
 
-public record Order (User buyer, LocalDateTime orderDate, List<OrderDetails> orderDetails) {
-    public Order {
-        Objects.requireNonNull(buyer);
-        Objects.requireNonNull(orderDate);
-        Objects.requireNonNull(orderDetails);
+@Data
+@Entity
+@EqualsAndHashCode
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private final LocalDateTime orderDate = LocalDateTime.now();
+    @NotEmpty
+    private User buyer;
+    @NotEmpty
+    private List<OrderDetails> orderDetails;
+
+    public Order(User buyer, List<OrderDetails> orderDetails) {
+        this.buyer = buyer;
+        this.orderDetails = orderDetails;
     }
 }
