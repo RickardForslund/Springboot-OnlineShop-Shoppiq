5e85fab5d2bbaf1a359f1e0dfec89498a6b1d0f0
Emily Inns <nyssa.inns@gmail.com>
2020-12-02 10:53:51 +0100

    Changed order details from record to class and added controller test class

--- src/main/java/com/shoppiq/entity/OrderDetails.java
+++ src/main/java/com/shoppiq/entity/OrderDetails.java
@@ -1,10 +1,44 @@
 package com.shoppiq.entity;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.Objects;
 
-public record OrderDetails (Long productId, String productName, double price, int quantity) {
-    public OrderDetails {
-        Objects.requireNonNull(productId);
-        Objects.requireNonNull(productName);
+@Data
+@Entity
+@EqualsAndHashCode
+@NoArgsConstructor
+public class OrderDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private static Long id;
+
+    @NotNull
+    Item item;
+
+    @Min(1)
+    @NotEmpty
+    int quantity;
+
+    double price;
+
+    public OrderDetails(Item item, @Min(1) @NotEmpty int quantity) {
+        this.item = item;
+        this.quantity = quantity;
+        this.price = item.price * quantity;
+    }
+
+
+    public OrderDetails() {
+
     }
 }
