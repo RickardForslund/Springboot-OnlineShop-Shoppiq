81b0be0de479193b3811f0e14f669ec1b3e9e028
toffe <toffe950301@gmail.com>
2020-12-02 13:14:35 +0100

    Added itemId and itemName and orderId with relations

--- src/main/java/com/shoppiq/entity/OrderDetails.java
+++ src/main/java/com/shoppiq/entity/OrderDetails.java
@@ -4,14 +4,9 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import java.util.Objects;
 
 @Data
 @Entity
@@ -20,25 +15,21 @@ import java.util.Objects;
 public class OrderDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private static Long id;
-
-    @NotNull
-    Item item;
-
+    private Long id;
+    Long itemId;
+    String itemName;
     @Min(1)
     @NotEmpty
     int quantity;
-
     double price;
 
+    @ManyToOne()
+    Order orderId;
+
     public OrderDetails(Item item, @Min(1) @NotEmpty int quantity) {
-        this.item = item;
+        this.itemId = item.getId();
+        this.itemName = item.getName();
         this.quantity = quantity;
-        this.price = item.price * quantity;
-    }
-
-
-    public OrderDetails() {
-
+        this.price = item.getPrice() * quantity;
     }
 }
