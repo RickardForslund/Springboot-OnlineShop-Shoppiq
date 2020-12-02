482b21cdd7b25f8ca4b5202e3ebc07cbf217d0b6
toffe <toffe950301@gmail.com>
2020-12-02 15:10:09 +0100

    Non working test

--- src/test/java/com/shoppiq/controller/OrderControllerTest.java
+++ src/test/java/com/shoppiq/controller/OrderControllerTest.java
@@ -1,4 +1,70 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.shoppiq.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shoppiq.entity.*;
+import com.shoppiq.enums.Category;
+import com.shoppiq.repository.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class OrderControllerTest {
-  
+
+    Orders order;
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+    @Autowired
+    OrderRepository orderRepository;
+    @Autowired
+    ItemRepository itemRepository;
+    @Autowired
+    OrderDetailsRepository orderDetailsRepository;
+    @Autowired
+    AddressRepository addressRepository;
+    @Autowired
+    UserRepository userRepository;
+
+
+    @BeforeEach
+    void setUp() {
+        //TODO test doubles
+        List<OrderDetails> orderDetailsList = new ArrayList<>();
+        Item item = new Item("Mög", 99, 1, Category.CLOTHES, "En Mög med ett stols märke.");
+        itemRepository.save(item);
+        OrderDetails orderDetail = new OrderDetails(item,1);
+        orderDetailsRepository.save(orderDetail);
+        Address address = new Address("swe","city","street","postal","apNum","c/o");
+        User user = new User("userName","password","e@mail.com","0", address);
+        userRepository.save(user);
+        Orders order = new Orders(user,orderDetailsList);
+        orderRepository.save(order);
+    }
+
+    @Test
+    void saveOrder() throws Exception { //TODO test fails. gives 400 bad request
+//        mockMvc.perform(post("/api/v1/order")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(objectMapper.writeValueAsString(order)))
+//                .andExpect(status().isOk())
+//                .andExpect(jsonPath("id").value(order.getId()));
+//                .andExpect(jsonPath("buyer").value(order.getBuyer()))
+//                .andExpect(jsonPath("orderDate").value(order.getOrderDate()))
+//                .andExpect(jsonPath("orderDetails").value(order.getOrderDetails()));
+    }
+
 }
