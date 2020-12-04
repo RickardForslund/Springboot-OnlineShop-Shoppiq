b4801bf0aef8c12bc1f49516c517aececb61de46
toffe <toffe950301@gmail.com>
2020-12-04 16:10:53 +0100

    Added a test getOrderById

--- src/test/java/com/shoppiq/controller/OrderControllerTest.java
+++ src/test/java/com/shoppiq/controller/OrderControllerTest.java
@@ -5,6 +5,7 @@ import com.shoppiq.entity.*;
 import com.shoppiq.enums.Category;
 import com.shoppiq.repository.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,9 +13,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.ws.rs.core.MediaType;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.Matchers.matchesRegex;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -55,16 +59,26 @@ class OrderControllerTest {
         orderRepository.save(order);
     }
 
+    @Disabled
     @Test
     void saveOrder() throws Exception { //TODO test fails. gives 400 bad request
-//        mockMvc.perform(post("/api/v1/order")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(objectMapper.writeValueAsString(order)))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("id").value(order.getId()));
-//                .andExpect(jsonPath("buyer").value(order.getBuyer()))
-//                .andExpect(jsonPath("orderDate").value(order.getOrderDate()))
-//                .andExpect(jsonPath("orderDetails").value(order.getOrderDetails()));
+        mockMvc.perform(post("/api/v1/order")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(order)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("id").value(order.getId()))
+                .andExpect(jsonPath("buyer").value(order.getBuyer()))
+                .andExpect(jsonPath("orderDate").value(order.getOrderDate()))
+                .andExpect(jsonPath("orderDetails").value(order.getOrderDetails()));
+    }
+
+    @Test
+    void getOrderById() throws Exception {
+        LocalDate dateNow = LocalDate.now();
+        mockMvc.perform(get("/api/v1/order/16"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("id").value(16))
+                .andExpect(jsonPath("orderDate").value(matchesRegex("^" + dateNow +".*"))); //TODO replace date when in live environment
     }
 
 }
