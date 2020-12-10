0e1a971ab4afae07bb304a88d31c1a38907c955f
toffe <toffe950301@gmail.com>
2020-12-10 14:59:24 +0100

    Made changes to work with frontend. Now has itemRepository which should be moved down to service if possible

--- src/main/java/com/shoppiq/controller/ItemController.java
+++ src/main/java/com/shoppiq/controller/ItemController.java
@@ -1,30 +1,49 @@
 package com.shoppiq.controller;
 
 import com.shoppiq.entity.Item;
+import com.shoppiq.repository.ItemRepository;
 import com.shoppiq.service.ItemService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
-@RestController
+@Controller //Frontend
+//@RestController //Insomnia
 @RequestMapping("/api/v1/item")
 public class ItemController {
 
     private final ItemService itemService;
+    private final ItemRepository itemRepository;
 
-    public ItemController(ItemService itemService) {
+    public ItemController(ItemService itemService, ItemRepository itemRepository) {
         this.itemService = itemService;
+        this.itemRepository = itemRepository;
     }
 
-    @PostMapping
-    public Item saveItem(@RequestBody Item item) {
-        return itemService.saveItem(item);
+    //<editor-fold desc="Create item">
+    @GetMapping("/create")
+    public String showCreateItemForm(Item item) {
+        return "create-item";
     }
 
+    @PostMapping("/add")
+    public String saveItem(Item item, BindingResult result, Model model) {
+        if (result.hasErrors())
+            return "error";
+        itemRepository.save(item);
+        return "home";
+    }
+    //</editor-fold>
+
+    //TODO add frontend
     @GetMapping
     public Iterable<Item> findAllItems() {
         return itemService.findAllItems();
     }
 
+    //TODO add frontend
     @GetMapping("/{id}")
     public Optional<Item> findItemById(@PathVariable Long id) {
         return itemService.findById(id);
