882d455a81fdaf4b5d08e278bc60e10bebce8412
Emily Inns <nyssa.inns@gmail.com>
2020-12-10 15:30:03 +0100

    Refactored registration and usercontroller to fit item but still nonfunctional

--- src/main/java/com/shoppiq/controller/UserController.java
+++ src/main/java/com/shoppiq/controller/UserController.java
@@ -1,21 +1,46 @@
 package com.shoppiq.controller;
 
+import com.shoppiq.entity.Item;
 import com.shoppiq.entity.User;
+import com.shoppiq.repository.ItemRepository;
+import com.shoppiq.repository.UserRepository;
+import com.shoppiq.service.ItemService;
 import com.shoppiq.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-@RestController
+//@RestController
+@Controller
 @RequestMapping("/api/v1/user")
 public class UserController {
 
+    private final UserRepository userRepository;
     private final UserService userService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserRepository userRepository) {
         this.userService = userService;
+        this.userRepository = userRepository;
     }
 
+    @GetMapping("/create")
+    public String showCreateUserForm(User user) {
+        return "create-item";
+    }
+
+
+    @PostMapping("/add")
+    public String saveUser(User user, BindingResult result, Model model) {
+        if (result.hasErrors())
+            return "error";
+        userRepository.save(user);
+        return "home";
+    }
+
+/*
     @PostMapping
     public User saveUser(@RequestBody User user) {
         return userService.saveUser(user);
@@ -26,8 +51,11 @@ public class UserController {
         return userService.findAllUsers();
     }
 
-    @GetMapping("/{id}")
-    public Optional<User> findUserById(@PathVariable Long id) {
-        return userService.findById(id);
-    }
+ */
+
+        @GetMapping("/{id}")
+        public Optional<User> findUserById (@PathVariable Long id){
+            return userService.findById(id);
+        }
+
 }
--- src/main/resources/static/js/registration.js
+++ src/main/resources/static/js/registration.js
@@ -1,6 +1,7 @@
 function postFetchForSignUp() {
     let nameInput = document.querySelector("#name")
     let usernameInput = document.querySelector("#username")
+    /*
     fetch('http://localhost:3000/users', { // First, we make a Post fetch request where we want to store our users.
         method: 'POST',
         headers: {
@@ -19,4 +20,6 @@ function postFetchForSignUp() {
            // slapUser(user)
             logOutButton()
         })
+
+     */
 }
