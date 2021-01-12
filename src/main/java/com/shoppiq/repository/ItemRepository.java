5fafe2211dd37ed538690c31077a2e2797a2e2b2
toffe <toffe950301@gmail.com>
2021-01-12 11:54:19 +0100

    Added findItemsBySellerId

--- src/main/java/com/shoppiq/repository/ItemRepository.java
+++ src/main/java/com/shoppiq/repository/ItemRepository.java
@@ -1,9 +1,13 @@
 package com.shoppiq.repository;
 
 import com.shoppiq.entity.Item;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ItemRepository extends CrudRepository<Item,Long> {
+
+    @Query("SELECT i FROM Item i WHERE i.sellerId = :id")
+    Iterable<Item> findItemsBySellerId(Long id);
 }
