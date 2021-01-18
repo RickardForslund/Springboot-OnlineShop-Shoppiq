3f9c3b4abbf3bcd92a4b294100fee877e86af6fd
toffe <toffe950301@gmail.com>
2021-01-18 15:17:11 +0100

    Changed sample data to work with authgroups

--- src/main/java/com/shoppiq/util/SampleDataGenerator.java
+++ src/main/java/com/shoppiq/util/SampleDataGenerator.java
@@ -1,9 +1,13 @@
 package com.shoppiq.util;
 
+import com.shoppiq.controller.UserController;
 import com.shoppiq.entity.*;
 import com.shoppiq.enums.Category;
 import com.shoppiq.repository.*;
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -16,6 +20,8 @@ public class SampleDataGenerator {
     UserRepository userRepository;
     ItemRepository itemRepository;
     OrderRepository orderRepository;
+    AuthGroupRepository authGroupRepository;
+    UserController userController;
 
     @PostConstruct
     public void initData() {
@@ -24,14 +30,28 @@ public class SampleDataGenerator {
         Address address3 = new Address("Country 3", "City 3", "Street 3", "3", "3", "none");
         Address address4 = new Address("Country 4", "City 4", "Street 4", "4", "4", "none");
         Address address5 = new Address("Country 5", "City 5", "Street 5", "5", "5", "none");
+        Address address6 = new Address("Country 6", "City 6", "Street 6", "6", "6", "none");
 
         User user1 = new User("user1", "pass1", "user1@email.com", "0701", address1);
         User user2 = new User("user2", "pass2", "user2@email.com", "0702", address2);
         User user3 = new User("user3", "pass3", "user3@email.com", "0703", address3);
         User user4 = new User("user4", "pass4", "user4@email.com", "0704", address4);
         User user5 = new User("user5", "pass5", "user5@email.com", "0705", address5);
-        var userList = Arrays.asList(user1, user2, user3, user4, user5);
-        userRepository.saveAll(userList);
+        User admin = new User("admin", "adminpass", "admin@email.com", "0706", address6);
+        authGroupRepository.save(new AuthGroup(admin.getUsername(), "ADMIN"));
+//        authGroupRepository.save(new AuthGroup(user1.getUsername(), "USER"));
+//        authGroupRepository.save(new AuthGroup(user2.getUsername(), "USER"));
+//        authGroupRepository.save(new AuthGroup(user3.getUsername(), "USER"));
+//        authGroupRepository.save(new AuthGroup(user4.getUsername(), "USER"));
+//        authGroupRepository.save(new AuthGroup(user5.getUsername(), "USER"));
+        userController.saveUser(user1);
+        userController.saveUser(user2);
+        userController.saveUser(user3);
+        userController.saveUser(user4);
+        userController.saveUser(user5);
+        userController.saveUser(admin);
+//        var userList = Arrays.asList(user1, user2, user3, user4, user5);
+//        userRepository.saveAll(userList);
 
         Item item1 = new Item("TV", 1999.00, 5, Category.ELECTRONICS, "Very descriptive text");
         Item item2 = new Item("VHS Player", 99.00, 1, Category.ELECTRONICS, "Very descriptive text");
@@ -41,11 +61,11 @@ public class SampleDataGenerator {
         var itemList = Arrays.asList(item1, item2, item3, item4, item5);
         itemRepository.saveAll(itemList);
 
-        OrderDetails orderDetails1 = new OrderDetails(item1.getId(), item1.getName(),item1.getPrice(), 1);
-        OrderDetails orderDetails2 = new OrderDetails(item2.getId(), item2.getName(),item2.getPrice(), 1);
-        OrderDetails orderDetails3 = new OrderDetails(item3.getId(), item3.getName(),item3.getPrice(), 1);
-        OrderDetails orderDetails4 = new OrderDetails(item4.getId(), item3.getName(),item4.getPrice(), 5);
-        OrderDetails orderDetails5 = new OrderDetails(item5.getId(), item3.getName(),item5.getPrice(), 2);
+        OrderDetails orderDetails1 = new OrderDetails(item1.getId(), item1.getName(), item1.getPrice(), 1);
+        OrderDetails orderDetails2 = new OrderDetails(item2.getId(), item2.getName(), item2.getPrice(), 1);
+        OrderDetails orderDetails3 = new OrderDetails(item3.getId(), item3.getName(), item3.getPrice(), 1);
+        OrderDetails orderDetails4 = new OrderDetails(item4.getId(), item3.getName(), item4.getPrice(), 5);
+        OrderDetails orderDetails5 = new OrderDetails(item5.getId(), item3.getName(), item5.getPrice(), 2);
         var orderDetailsList = Arrays.asList(orderDetails1, orderDetails2, orderDetails3, orderDetails4, orderDetails5);
         var electronicsOrderList = Arrays.asList(orderDetails1, orderDetails2);
         var clothesOrderList = Arrays.asList(orderDetails3, orderDetails4);
@@ -62,5 +82,4 @@ public class SampleDataGenerator {
         var ordersList = Arrays.asList(order1, order2, order3, order4);
         orderRepository.saveAll(ordersList);
     }
-
 }
