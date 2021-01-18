7e445c3e94967a7868e5220372b01acd5459d03c
toffe <toffe950301@gmail.com>
2021-01-18 15:17:48 +0100

    removed / on the names

--- src/main/java/com/shoppiq/view/MvcConfig.java
+++ src/main/java/com/shoppiq/view/MvcConfig.java
@@ -8,16 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("/home");
-        registry.addViewController("/").setViewName("/home");
-        registry.addViewController("/logout").setViewName("/home");
-        registry.addViewController("/application").setViewName("/application");
-        registry.addViewController("/admin").setViewName("/admin");
-        registry.addViewController("/testadmin").setViewName("/testadmin");
-        registry.addViewController("/login").setViewName("/login");
-        registry.addViewController("/testlogin").setViewName("/testlogin");
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/logout").setViewName("home");
+        registry.addViewController("/application").setViewName("application");
+        registry.addViewController("/admin").setViewName("admin");
+        registry.addViewController("/testadmin").setViewName("testadmin");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/testlogin").setViewName("testlogin");
        // registry.addViewController("/api/v1/user/create").setViewName("/api/v1/user/create");
-        registry.addViewController("/error").setViewName("/error");
-        registry.addViewController("/create_item").setViewName("/create_item");
+        registry.addViewController("/error").setViewName("error");
+        registry.addViewController("/create_item").setViewName("create_item");
     }
 }
