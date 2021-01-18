a96f9d06d02c425a75630955178033a8017552ae
toffe <toffe950301@gmail.com>
2021-01-18 18:26:26 +0100

    Added service layer for authgroup

--- src/main/java/com/shoppiq/service/AuthGroupService.java
+++ src/main/java/com/shoppiq/service/AuthGroupService.java
@@ -1,2 +1,19 @@
-package com.shoppiq.service;public class AuthGroupService {
+package com.shoppiq.service;
+
+import com.shoppiq.entity.AuthGroup;
+import com.shoppiq.repository.AuthGroupRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthGroupService {
+
+    private final AuthGroupRepository authGroupRepository;
+
+    public AuthGroupService(AuthGroupRepository authGroupRepository) {
+        this.authGroupRepository = authGroupRepository;
+    }
+
+    public AuthGroup save(String username, String authGroup) {
+        return authGroupRepository.save(new AuthGroup(username, authGroup));
+    }
 }
