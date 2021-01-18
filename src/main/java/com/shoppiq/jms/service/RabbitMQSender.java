6222c8315b12fbb4a5420636a2fa0f0686fc1ae7
toffe <toffe950301@gmail.com>
2021-01-18 23:07:17 +0100

    Added constructors for all entities

--- src/main/java/com/shoppiq/jms/service/RabbitMQSender.java
+++ src/main/java/com/shoppiq/jms/service/RabbitMQSender.java
@@ -1,5 +1,6 @@
 package com.shoppiq.jms.service;
 
+import com.shoppiq.entity.*;
 import com.shoppiq.jms.model.Employee;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,4 +24,35 @@ public class RabbitMQSender {
         System.out.println("Send msg = " + company);
 
     }
+
+    public void send(Item item) {
+        rabbitTemplate.convertAndSend(exchange, routingkey, item);
+        System.out.println("Sending message");
+    }
+
+    public void send(User user) {
+        rabbitTemplate.convertAndSend(exchange, routingkey, user);
+        System.out.println("Sending message");
+    }
+
+    public void send(Address address) {
+        rabbitTemplate.convertAndSend(exchange, routingkey, address);
+        System.out.println("Sending message");
+    }
+
+    public void send(Orders orders) {
+        rabbitTemplate.convertAndSend(exchange, routingkey, orders);
+        System.out.println("Sending message");
+    }
+
+    public void send(OrderDetails orderDetails) {
+        rabbitTemplate.convertAndSend(exchange, routingkey, orderDetails);
+        System.out.println("Sending message");
+    }
+
+    public void send(AuthGroup authGroup) {
+        rabbitTemplate.convertAndSend(exchange, routingkey, authGroup);
+        System.out.println("Sending message");
+    }
+
 }
