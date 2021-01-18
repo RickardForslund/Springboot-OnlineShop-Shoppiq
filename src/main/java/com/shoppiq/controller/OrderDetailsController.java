890fc6feeaed0b7d356d435f7575d6015a0ee7db
Emily Inns <nyssa.inns@gmail.com>
2021-01-18 20:50:53 +0100

    Removed password from user list and added groundwork for show orderdetails/buy page

--- src/main/java/com/shoppiq/controller/OrderDetailsController.java
+++ src/main/java/com/shoppiq/controller/OrderDetailsController.java
@@ -2,11 +2,14 @@ package com.shoppiq.controller;
 
 import com.shoppiq.entity.OrderDetails;
 import com.shoppiq.service.OrderDetailsService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-@RestController
+@Controller
 @RequestMapping("/api/v1/orderdetails")
 public class OrderDetailsController {
 
@@ -29,6 +32,30 @@ public class OrderDetailsController {
     @GetMapping("/{id}")
     public Optional<OrderDetails> findOrderDetailsById(@PathVariable Long id) {
         return orderdetailsService.findById(id);
+    }
+
+    @PostMapping("/add")
+    public String saveOrderDetails(@ModelAttribute("orderDetails") OrderDetails orderDetails, BindingResult result, Model model) {
+        if (result.hasErrors())
+            return "error";
+        orderdetailsService.saveOrderDetails(orderDetails);
+        model.addAttribute("orderDetails",orderDetails);
+        return "orderDetails";
+    }
+
+
+    @GetMapping("/view/{id}")
+    public String showOrderDetailsById(Model model, @PathVariable Long id) {
+        OrderDetails orderDetails = null;
+
+        try {
+            orderDetails = orderdetailsService.findById(id).get();
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", "Item not found");
+        }
+
+        model.addAttribute("orderDetails",orderDetails);
+        return "orderDetails";
     }
 
 }
